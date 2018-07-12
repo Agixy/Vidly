9a788e0624df22170ee47e1434e2b3335d4ecb73
Your Name <you@example.com>
2018-07-12 09:40:39 +0200

    Routes Vid 12-14

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -20,21 +20,10 @@ namespace Vidly.Controllers
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });  // index-nazwa akcji(metody), Home-kontroler, parametry routingu - po czym ma szukać, wyswietlac
         }
 
-        public ActionResult Edit(int id)
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content("id=" + id);
-        }
-
-        //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format($"pageIndex={pageIndex}&sortBy={sortBy}"));
+            return Content(year + "/" + month);
         }
     }
 }
